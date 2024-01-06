28ff4dec6a0a1d7d8d8f4323348546bd6210f739
VampireVVB <129896603+VampireVVB@users.noreply.github.com>
2024-01-06 16:29:44 +0530

    Add files via upload

---
+++ 220953624/Lab-1/Q-2/Q-2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("I"); 
+            comboBox1.Items.Add("II"); 
+            comboBox1.Items.Add("III");
+            comboBox1.Items.Add("IV");
+        }
+    }
+}
